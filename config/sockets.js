e5ad277b2bdb7bf0b7b60ba55b6e1ed66b4e9ffa
PlutoA713N <mallachandu99@gmail.com>
2024-04-18 06:38:21 +0530

    Fix Config

--- config/sockets.js
+++ config/sockets.js
@@ -29,6 +29,7 @@ module.exports.sockets = {
 
   // transports: [ 'websocket' ],
 
+  onlyAllowOrigins: [],
 
   /***************************************************************************
   *                                                                          *
@@ -42,13 +43,13 @@ module.exports.sockets = {
   *                                                                          *
   ***************************************************************************/
 
-  // beforeConnect: function(handshake, proceed) {
-  //
-  //   // `true` allows the socket to connect.
-  //   // (`false` would reject the connection)
-  //   return proceed(undefined, true);
-  //
-  // },
+  beforeConnect: function(handshake, proceed) {
+  
+    // `true` allows the socket to connect.
+    // (`false` would reject the connection)
+    return proceed(undefined, true);
+  
+  },
 
 
   /***************************************************************************
