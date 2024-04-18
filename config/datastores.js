7c4bc685cfe366bef2ed3ad31e63f95e963eb329
PlutoA713N <mallachandu99@gmail.com>
2024-04-18 11:56:43 +0530

    Update Host name

--- config/datastores.js
+++ config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mysql',
-    url: 'mysql://root:new_password@localhost:3306/quizDb',
+    url: 'mysql://root:new_password@Mr:3306/quizDb',
     connectTimeout: 20000,
 
 
