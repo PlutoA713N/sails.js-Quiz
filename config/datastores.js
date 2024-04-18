623900e95729f840b4692396c09d7de8e54f1010
PlutoA713N <mallachandu99@gmail.com>
2024-04-18 15:37:54 +0530

    Increase timeOut

--- config/datastores.js
+++ config/datastores.js
@@ -50,7 +50,7 @@ module.exports.datastores = {
     ***************************************************************************/
     adapter: 'sails-mysql',
     url: 'mysql://user0.1:user0.1@10.10.2.161/quizDb',
-    connectTimeout: 30000,
+    connectTimeout: 70000,
 
 
   },
