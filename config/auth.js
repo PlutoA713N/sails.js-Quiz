3feb7052adcf4a004bd045555f6326db85ab50ab
PlutoA713N <mallachandu99@gmail.com>
2024-04-21 23:50:18 +0530

    Add AuthConfig

---
+++ config/auth.js
@@ -0,0 +1,16 @@
+// const { auth } = require('express-openid-connect');
+
+// const config = {
+//     authRequired: false,
+//     auth0Logout: true,
+//     secret: 'WcymIx50tSaD5HrkoGLQ50PLf1jVqGN4efIDIbxoDyfg_hxm6LUYUGg7kLRzPQhs',
+//     baseURL: 'http://localhost:1337',
+//     clientID: 'lgSTjHqQsVw6D7h2wUOaoVwUy40Bu7OA',
+//     issuerBaseURL: 'https://dev-3e5w3b8uhxdlo41m.us.auth0.com'
+// }
+
+// const authMiddleware = auth(config);
+
+// module.exports = {
+//     authMiddleware: authMiddleware
+// };
